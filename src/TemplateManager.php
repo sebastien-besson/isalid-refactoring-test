b3f676a789dbfb074e977efbfd54ac2e94d993f9
sbesson <sbesson@clever-age.com>
2021-10-08 17:35:49 +0200

    Inject and execute the QuoteHelperRegistry into TemplateManager and clean the rest

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -2,11 +2,11 @@
 
 class TemplateManager
 {
-    private $applicationContext;
+    private $quoteHelperRegistry;
 
     public function __construct()
     {
-        $this->applicationContext = new ApplicationContext();
+        $this->quoteHelperRegistry = new QuoteHandlerRegistry();
     }
 
     public function getTemplateComputed(Template $tpl, array $data)
@@ -22,63 +22,8 @@ class TemplateManager
         return $replaced;
     }
 
-    private function computeText($text, array $data)
+    private function computeText($text, array $data): string
     {
-        $quote = (isset($data['quote']) and $data['quote'] instanceof Quote) ? $data['quote'] : null;
-
-        if ($quote)
-        {
-            $_quoteFromRepository = QuoteRepository::getInstance()->getById($quote->id);
-            $usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
-            $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
-
-            if(strpos($text, '[quote:destination_link]') !== false){
-                $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
-            }
-
-            $containsSummaryHtml = strpos($text, '[quote:summary_html]');
-            $containsSummary     = strpos($text, '[quote:summary]');
-
-            if ($containsSummaryHtml !== false || $containsSummary !== false) {
-                if ($containsSummaryHtml !== false) {
-                    $text = str_replace(
-                        '[quote:summary_html]',
-                        Quote::renderHtml($_quoteFromRepository),
-                        $text
-                    );
-                }
-                if ($containsSummary !== false) {
-                    $text = str_replace(
-                        '[quote:summary]',
-                        Quote::renderText($_quoteFromRepository),
-                        $text
-                    );
-                }
-            }
-
-            (strpos($text, '[quote:destination_name]') !== false) and $text = str_replace('[quote:destination_name]',$destinationOfQuote->countryName,$text);
-        }
-
-        if (isset($destination))
-            $text = str_replace('[quote:destination_link]', $usefulObject->url . '/' . $destination->countryName . '/quote/' . $_quoteFromRepository->id, $text);
-        else
-            $text = str_replace('[quote:destination_link]', '', $text);
-
-
-        $user = $this->getUser($data['user']);
-
-        (strpos($text, '[user:first_name]') !== false) and $text = str_replace('[user:first_name]'       , ucfirst(mb_strtolower($user->firstname)), $text);
-
-
-        return $text;
-    }
-
-    private function getUser($user)
-    {
-        if (isset($user) && $user instanceof User) {
-            return $user;
-        }
-
-        return $this->applicationContext->getCurrentUser();
+        return $this->quoteHelperRegistry->execute($text, $data);
     }
 }
